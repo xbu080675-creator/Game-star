ec615644667524eda16ede891308da88ed391f79
PocketSpace <xbu080675@gmail.com>
2026-09-14 15:05:03 +0800

    feat: define semantic game capability ids

---
+++ app/src/main/java/com/xbu/esportscenter/core/capability/GameCapabilityIds.java
@@ -0,0 +1,18 @@
+package com.xbu.esportscenter.core.capability;
+
+/** Stable platform-neutral capability IDs used by Game Star Box. */
+public final class GameCapabilityIds {
+    private GameCapabilityIds() {}
+
+    public static final String NETWORK_BOOST = "game.network.boost";
+    public static final String PERFORMANCE_BOOST = "game.performance.boost";
+    public static final String MEMORY_CLEANUP = "game.memory.cleanup";
+    public static final String INTERRUPTION_SHIELD = "game.interruption.shield";
+
+    public static final String FPS_MONITOR = "overlay.fps_monitor";
+    public static final String CROSSHAIR = "overlay.crosshair";
+    public static final String FAN_CONTROL = "device.fan_control";
+    public static final String CHARGE_SEPARATION = "device.charge_separation";
+    public static final String SHOULDER_MAPPING = "input.shoulder_mapping";
+    public static final String SCREEN_RECORD = "capture.screen_record";
+}
