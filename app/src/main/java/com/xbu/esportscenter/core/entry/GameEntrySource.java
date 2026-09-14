f0f0a6b6b0ae39b0c1ef1ba681eea5724d5a02db
PocketSpace <xbu080675@gmail.com>
2026-09-14 13:31:14 +0800

    feat: add platform-neutral game entry source

---
+++ app/src/main/java/com/xbu/esportscenter/core/entry/GameEntrySource.java
@@ -0,0 +1,8 @@
+package com.xbu.esportscenter.core.entry;
+
+/** Platform-neutral source of a request to enter the Game Star Box game surface. */
+public enum GameEntrySource {
+    REDMAGIC_COMPETITIVE_SWITCH,
+    APP_LAUNCHER,
+    UNKNOWN
+}
